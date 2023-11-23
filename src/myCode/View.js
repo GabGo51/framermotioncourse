379b8cd906e7853e8270d09de54cade2dd249c8a
GabGo51 <snow.gabo51@gmail.com>
2023-11-23 16:13:42 -0500

    made cool header

--- src/myCode/View.js
+++ src/myCode/View.js
@@ -1,49 +1,80 @@
 import React from "react";
 import styled from "styled-components";
-import { motion, AnimatePresence, useAnimationControls, inView, animate } from "framer-motion";
+import {motion} from "framer-motion";
 import img from "../img/eva2.PNG";
 const View = () => {
 
   const initial = {
-    opacity:0,
-    x:50
-  }
+    opacity: 0,
+    x: 50,
+  };
 
   const h1animation = {
-    opacity:1, 
-    x:0
-  }
+    opacity: 1,
+    x: 0,
+  };
 
   const panimation = {
-    opacity:1,
-    y:50,
-    x:0,
-  }
+    opacity: 1,
+    y: 50,
+    x: 0,
+  };
+
+  const imageInitial = {
+    opacity: 0,
+    x: -50,
+  };
+
+  const imageAnimation = {
+    opacity: 1,
+    x: 0,
+  };
+
+  const AnimatedImage = () => (
+    <motion.img
+      initial={imageInitial}
+      whileInView={imageAnimation}
+      transition={{ duration: 2 }}
+      src={img}
+    />
+  );
 
   return (
     <Container>
-      
       <div className="section black">
         <div className="frame">
           <div>
             <motion.h1
               whileInView={h1animation}
               initial={initial}
-              transition={{ duration: 1}}
-              exit={{
-                opacity:0
-              }}
+              transition={{ duration: 1 }}
             >
               TEST 1
             </motion.h1>
-            <motion.p initial = {initial} whileInView={panimation} transition={{ duration: 1}}>this is sum random text hihi</motion.p>
+            <motion.p
+              initial={initial}
+              whileInView={panimation}
+              transition={{ duration: 1 }}
+            >
+              this is sum random text hihi
+            </motion.p>
           </div>
-          <img src={img} />
+          <AnimatedImage/>
         </div>
       </div>
-      
+
       <div className="section white">
-        <div className="frame"></div>
+        <header>
+          <div>
+            <img src={img}/>
+            <ul>
+              <motion.li initial={{opacity:0}} whileInView={{opacity:1}} transition={{duration:1, delay:.6}}>HELLO</motion.li>
+              <motion.li initial={{opacity:0}} whileInView={{opacity:1}} transition={{duration:1, delay:.8}}>HELLO</motion.li>
+              <motion.li initial={{opacity:0}} whileInView={{opacity:1}} transition={{duration:1, delay:1}}>HELLO</motion.li>
+            </ul>
+          </div>
+          <motion.div className="bottom-bar" initial={{scaleX:0}} whileInView={{scaleX:1}} transition={{duration:1}}></motion.div>
+        </header>
       </div>
       <div className="section black">
         <div className="frame"></div>
@@ -62,6 +93,42 @@ const Container = styled.div`
   img {
     width: 300px;
     height: 330px;
+  }
+
+  header {
+    width: 90vw;
+    div{
+      display: flex;
+      justify-content: space-between;
+      margin-bottom: 10px;
+    }
+
+    img{
+      width: 50px;
+      height: 50px;
+      border-radius: 50%;
+    }
+    ul{
+      display: flex;
+      list-style-type: none;
+
+      li{
+        cursor: pointer;
+        margin-left: 40px;
+        transition: 400ms;
+        &:hover{
+          scale: 1.1;
+        }
+      }
+    }
+
+
+    .bottom-bar{
+      height: 1px;
+      background-color: black;
+      width: 100%;
+      transform-origin: left center;
+    }
   }
 
   .section {
